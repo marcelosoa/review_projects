2738332cc03cebcb4aa482caf705e44af6931caa
Marcelo <marcelosoaresinc@gmail.com>
2023-06-16 22:14:12 -0300

    new login

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -14,129 +14,90 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0XFF3D3D3D),
       body: SafeArea(
-        child: Container(
-          decoration: const BoxDecoration(color: Color(0XFFF5F5F5)),
+        child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               const SizedBox(height: 80),
-              const Padding(
-                padding: EdgeInsets.all(20),
+              Padding(
+                padding: const EdgeInsets.all(20),
                 child: Column(
-                  children: <Widget>[
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
                     Text(
-                      "Login",
-                      style: TextStyle(color: Colors.black, fontSize: 40),
+                      'Login',
+                      style: TextStyle(color: Colors.white, fontSize: 40),
                     ),
-                    SizedBox(height: 24),
+                    SizedBox(height: 15),
                     Text(
-                      "Welcome Back",
-                      style: TextStyle(color: Colors.black, fontSize: 15),
+                      'Welcome back',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
                     ),
                   ],
                 ),
               ),
-              const Spacer(), // Espaço flexível para empurrar o conteúdo para o topo
-              Transform.translate(
-                offset: const Offset(0, -1), // Ajuste vertical para cima
-                child: Container(
-                  color: const Color(0XFF3D3D3D),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 15, vertical: 100),
-                  margin:
-                      const EdgeInsets.all(16), // Espaçamento em todos os lados
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: <Widget>[
-                        Container(
-                          color: const Color(0XFFF5F5F5),
-                          child: Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.stretch,
-                              children: <Widget>[
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 1),
-                                  child: TextFormField(
-                                    decoration: InputDecoration(
-                                      filled: true,
-                                      fillColor: const Color(0XFFD9D9D9),
-                                      hoverColor: const Color(0XFFD9D9D9),
-                                      enabledBorder: OutlineInputBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(3),
-                                        borderSide: const BorderSide(
-                                          color: Color(0xFFD9D9D9),
-                                        ),
-                                      ),
-                                      hintText: 'email@example.com',
-                                      suffixIcon: const Icon(Icons.email),
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(height: 25),
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 1),
-                                  child: TextFormField(
-                                    decoration: InputDecoration(
-                                      filled: true,
-                                      fillColor: const Color(0XFFD9D9D9),
-                                      hoverColor: const Color(0XFFD9D9D9),
-                                      enabledBorder: OutlineInputBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(3),
-                                        borderSide: const BorderSide(
-                                          color: Color(0xFFD9D9D9),
-                                        ),
-                                      ),
-                                      hintText: 'password',
-                                      suffixIcon: const Icon(Icons.remove_red_eye),
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
+              const SizedBox(height: 25),
+              Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(60),
+                    topRight: Radius.circular(60),
+                  ),
+                  color: Color(0XFFF5F5F5),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 25),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          suffixIcon: Icon(Icons.email),
+                          hintText: 'email@example.com',
                         ),
-                        const SizedBox(
-                            height:
-                                16), // Espaçamento entre o formulário e os textos
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            TextButton(
-                              onPressed: () {},
-                              child: const Text(
-                                'Criar conta',
-                                style: TextStyle(color: Colors.white),
+                      ),
+                      const SizedBox(height: 15),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: 'password',
+                          suffixIcon: Icon(Icons.password),
+                        ),
+                      ),
+                      const SizedBox(height: 25),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text('Criar conta'),
+                          Text('Esqueci minha senha'),
+                        ],
+                      ),
+                      const SizedBox(height: 25),
+                      GestureDetector(
+                        onTap: () {
+                          print('cliquei');
+                        },
+                        child: Container(
+                          height: 70,
+                          margin: const EdgeInsets.symmetric(horizontal: 50),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: const Color(0XFFD9D9D9),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'conectar',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
                               ),
                             ),
-                            const SizedBox(width: 20),
-                            TextButton(
-                              onPressed: () {},
-                              child: const Text(
-                                'Esqueci minha senha',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            ),
-                          ],
-                        ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            primary: const Color(0XFFF5F5F5),
-                          ),
-                          child: const Text(
-                            'Conectar',
-                            style: TextStyle(color: Colors.black),
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                      const SizedBox(height: 25),
+                    ],
                   ),
                 ),
               ),
